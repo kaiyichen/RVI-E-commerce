dfc20b073dce239fc309d3ae651052e48645f1ca
kaiyichen <chenkaiyi01@gmail.com>
2022-06-24 19:40:36 +0800

    no message

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./tailwind.css', './index.html','./style.css'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'class',
+}
